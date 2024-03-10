99229df6093d56bbf39a2327c71fc5a53c19a414
Logan McKerlich <logan.mckerlich@gmail.com>
2024-03-10 10:09:36 -0700

    logging

--- cbb_shirts.py
+++ cbb_shirts.py
@@ -5,7 +5,16 @@ import traceback
 import yaml
 import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+
 logger =  logging.getLogger()
+
 
 
 class cbb:
